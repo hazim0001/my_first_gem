d3060980e5ce79cfee8cd7c423c823d7f6ffb096
hazim0001 <hazim_alsayed@hotmail.com>
2020-12-24 19:28:44 +0200

    percent_of function

--- lib/my_first_gem.rb
+++ lib/my_first_gem.rb
@@ -1,8 +1,16 @@
-# frozen_string_literal: true
+ # frozen_string_literal: true
 
 require_relative "my_first_gem/version"
 
 module MyFirstGem
   class Error < StandardError; end
   # Your code goes here...
+
+  def percent_of(number)
+    (self/100) * number
+  end
+end
+
+class Integer
+  include MyFirstGem
 end
